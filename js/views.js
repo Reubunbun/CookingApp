a0e81e6f14787fde249d6820bd98e286111ee23e
Reuben <rp506@exeter.ac.uk>
2021-01-12 17:01:31 +0000

    Create views.js

---
+++ js/views.js
@@ -0,0 +1,54 @@
+//Main view
+App.Views.App = Backbone.View.extend({
+  initialize: function(){
+    let addLocationView  = new App.Views.AddLocation({collection: App.locations});
+    let allLocationsView = new App.Views.Locations({collection: App.locations});
+    allLocationsView.render();
+    $("#locationList").append(allLocationsView.el);
+  }
+});
+
+//Single location
+App.Views.Location = Backbone.View.extend({
+  tagName: "li",
+
+  render() {
+    this.$el.html( this.model.get("name") );
+    return this;
+  }
+});
+
+//Collection of locations
+App.Views.Locations = Backbone.View.extend({
+  tagName: "ul",
+
+  initialize() {
+    this.collection.on("add", this.addOne, this);
+  },
+
+  render() {
+    this.collection.each(this.addOne, this);
+  },
+
+  addOne(location) {
+    let locationView = new App.Views.Location({model: location});
+    this.$el.append(locationView.render().el);
+    return this;
+  }
+});
+
+//Add location button
+App.Views.AddLocation = Backbone.View.extend({
+  el: "#searchButton",
+
+  events: {
+    "click": "addLocation"
+  },
+
+  addLocation(e) {
+    e.preventDefault();
+    this.collection.add({
+      "name" : $("#searchBox").val()
+    });
+  }
+});
