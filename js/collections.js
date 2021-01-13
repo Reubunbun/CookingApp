11f0d9d443a54a8e059e78171485c7c70be285f5
Reuben <rp506@exeter.ac.uk>
2021-01-13 17:35:15 +0000

    Update collections.js

--- js/collections.js
+++ js/collections.js
@@ -1,4 +1,15 @@
-App.Collections.Meals = Backbone.Collection.extend({
+//Collection of meals that the user has saved
+App.Collections.SavedMeals = Backbone.Collection.extend({
+  model: App.Models.Meal
+});
+
+//Collection of meals that are returned from a search
+App.Collections.SearchedMeals = Backbone.Collection.extend({
   model: App.Models.Meal,
-  url: ""
+  requestUrl: "https://www.themealdb.com/api/json/v1/1/search.php?s=",
+  url: "",
+
+  parse(response) {
+    if ("meals" in response) return response.meals;
+  }
 });
