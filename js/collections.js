b422507a07d0bad951b4be2c9fd8e64ea21a333b
Reuben <rp506@exeter.ac.uk>
2021-01-13 15:27:04 +0000

    Update collections.js

--- js/collections.js
+++ js/collections.js
@@ -1,4 +1,4 @@
-App.Collections.Locations = Backbone.Collection.extend({
-  model: App.Models.Location,
-  url: "https://www.metaweather.com/api/location/search/?query=san"
+App.Collections.Meals = Backbone.Collection.extend({
+  model: App.Models.Meal,
+  url: ""
 });
