669053af22b811c78602f0e62e2beb1e92f234c4
Reuben <rp506@exeter.ac.uk>
2021-01-13 17:35:10 +0000

    Update views.js

--- js/views.js
+++ js/views.js
@@ -1,15 +1,16 @@
 //Main view
 App.Views.App = Backbone.View.extend({
   initialize: function(){
-    let addMealView  = new App.Views.AddMeal({collection: App.meals});
-    let allMealsView = new App.Views.Meals({collection: App.meals});
-    allMealsView.render();
-    $("#mealList").append(allMealsView.el);
+    let searchBoxView = new App.Views.SearchBox({collection: App.searchedMeals});
+    let searchedMealsView = new App.Views.SearchedMeals({collection: App.searchedMeals});
+    let savedMealsView = new App.Views.SavedMeals({collection: App.savedMeals});
+    savedMealsView.render();
+    $("#mealList").append(savedMealsView.el);
   }
 });
 
-//Single Meal
-App.Views.Meal = Backbone.View.extend({
+//Single Saved Meal
+App.Views.SavedMeal = Backbone.View.extend({
   tagName: "li",
 
   template: template("mealTemplate"),
@@ -29,48 +30,67 @@ App.Views.Meal = Backbone.View.extend({
   }
 });
 
-//Collection of Meals
-App.Views.Meals = Backbone.View.extend({
+//Collection Of Saved Meals
+App.Views.SavedMeals = Backbone.View.extend({
   tagName: "ul",
 
   initialize() {
     this.collection.on("add", this.addOne, this);
   },
-
   render() {
     this.collection.each(this.addOne, this);
   },
-
   addOne(meal) {
-    let mealView = new App.Views.Meal({model: meal});
+    let mealView = new App.Views.SavedMeal({model: meal});
     this.$el.append(mealView.render().el);
     return this;
   },
 
 });
 
-//Add Meal button
-App.Views.AddMeal = Backbone.View.extend({
-  el: "#searchButton",
-
+//Searchbox view
+App.Views.SearchBox = Backbone.View.extend({
+  el: "#searchBox",
   events: {
-    "click": "addMeal"
+    "keyup": "search"
   },
 
-  addMeal(e) {
-    e.preventDefault();
-    let newMeal = new App.Models.Meal(
-      { name: $("#searchBox").val() }, {validate: true}
-    );
-    let bExistsInCollection = this.collection.findWhere({name: newMeal.get("name")});
+  search(){
+    this.collection.url = this.collection.requestUrl + this.$el.val();
+    this.collection.each(
+      function(model){model.destroy();
+    });
+    this.collection.reset();
+    this.collection.fetch();
+  }
+});
 
-    if (!newMeal.validationError) {
-      if (!bExistsInCollection)
-        this.collection.add(newMeal);
-      else
-        console.log("Meal already in list");
-    } else {
-      console.log(newMeal.validationError);
-    }
+//Single search result
+App.Views.SearchedMeal = Backbone.View.extend({
+  tagName: "li",
+
+  initialize() {
+    this.model.on("destroy", this.remove, this);
+  },
+  render() {
+    this.$el.html( this.model.get("strMeal") );
+    return this;
+  },
+  remove(){
+    this.$el.remove();
+  }
+});
+
+//Collection of search results
+App.Views.SearchedMeals = Backbone.View.extend({
+  el: "#searchResults",
+
+  initialize() {
+    this.collection.on("add", this.addOne, this);
+  },
+  addOne(meal) {
+    let mealView = new App.Views.SearchedMeal({model: meal});
+    this.$el.append(mealView.render().el);
+    return this;
   }
 });
