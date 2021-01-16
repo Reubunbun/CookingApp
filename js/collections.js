98ec7a1b9f9581d185d90b5b495212e663b016de
Reubunbun <Reuben.Luke.P@Gmail.com>
2021-01-16 13:57:04 +0000

    Update collections.js

--- js/collections.js
+++ js/collections.js
@@ -1,11 +1,12 @@
-//Collection of meals that the user has saved
-App.Collections.SavedMeals = Backbone.Collection.extend({
-  model: App.Models.Meal
+//Collection of Recipes that the user has saved
+App.Collections.SavedRecipes = Backbone.Collection.extend({
+  model: App.Models.Recipe,
+  unique: true
 });
 
-//Collection of meals that are returned from a search
-App.Collections.SearchedMeals = Backbone.Collection.extend({
-  model: App.Models.Meal,
+//Collection of Recipes that are returned from a search
+App.Collections.SearchedRecipes = Backbone.Collection.extend({
+  model: App.Models.Recipe,
   requestUrl: "https://www.themealdb.com/api/json/v1/1/search.php?s=",
   url: "",
 
